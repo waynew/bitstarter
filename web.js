4e7ac57a78c593cc7fe3c02789d3f624cc6790e5
Wayne Werner <waynejwerner@gmail.com>
2013-07-05 09:05:59 -0500

    Added bufferized sending

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World, too!');
+    var data = fs.readFileSync('index.html');
+    response.send(data.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
